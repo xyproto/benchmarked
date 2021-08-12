5c62071b2c5e7fa1c46f2fd9ca5b445166fd8876
Alexander F. Rødseth <rodseth@gmail.com>
2021-08-12 19:18:23 +0200

    Update the benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 go test -bench=. "$@" | tee bench.out
-head -4 bench.out
-grep 'ns/op' bench.out | sort -r -n -k3
+head -4 bench.out > tmp.out
+grep 'ns/op' bench.out | sort -r -n -k3 >> tmp.out
+tail -2 bench.out >> tmp.out
+mv -f tmp.out bench.out
